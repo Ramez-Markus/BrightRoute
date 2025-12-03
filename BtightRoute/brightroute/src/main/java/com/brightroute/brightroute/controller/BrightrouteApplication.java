a1bf8a3ba58a0b82c42538ca9a9874c3d86e036f
Basmola.innovation <Basmola.basmalaamr9@gmail.com>
2025-12-03 16:01:37 +0200

    Moved BrightrouteApplication.java into newly created controller folder and added service and repository folders

--- BtightRoute/brightroute/src/main/java/com/brightroute/brightroute/controller/BrightrouteApplication.java
+++ BtightRoute/brightroute/src/main/java/com/brightroute/brightroute/controller/BrightrouteApplication.java
@@ -1,4 +1,4 @@
-package com.brightroute.brightroute;
+package com.brightroute.brightroute.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
